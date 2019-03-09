414bdc4e31c0fcae0cedeee1ebacd4542e002d6c
Ameed Jamous <a.jamous@telecomsxchange.com>
2019-03-09 02:50:26 +0200

    Update sms.go

--- sms.go
+++ sms.go
@@ -1,12 +1,26 @@
-body := strings.NewReader(`From=19876543212&To=13216549878&Body=Test SMS from Restcomm&StatusCallback=http://status.callback.url`)
-req, err := http.NewRequest("POST", "https://<accountSid>:<authToken>@cloud.restcomm.com/restcomm/2012-04-24/Accounts/<accountSid>/SMS/Messages", body)
-if err != nil {
-	// handle err
-}
-req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+package main
 
-resp, err := http.DefaultClient.Do(req)
-if err != nil {
-	// handle err
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	client := &http.Client{}
+	var data = []byte(`{From=19876543212&To=13216549878&Body=Test SMS from Restcomm&StatusCallback=http://status.callback.url}`)
+	req, err := http.NewRequest("POST", "https://<accountSid>:<authToken>@cloud.restcomm.com/restcomm/2012-04-24/Accounts/<accountSid>/SMS/Messages", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", bodyText)
 }
-defer resp.Body.Close()
